7f1823458e36c337a654b59951cccc3555c13910
Simon Weniger <hello@simonweniger.com>
2021-05-27 10:25:12 +0200

    fixed card shapes based off theme

--- docs/src/components/cards/NFT.js
+++ docs/src/components/cards/NFT.js
@@ -2,22 +2,22 @@ import React from "react";
 
 function NFT() {
   return (
-    <div className="flex space-x-4">
+    <div className="flex space-x-12">
       <div className="card bordered max-w-xs">
         <div className="card-user">
             <div className="avatar">
               <div className="rounded-full w-7 h-7">
-                <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png" />
+                <img src="https://source.unsplash.com/random/300x300" />
               </div>
               <p className="ml-2 body-large text-base-content">@username</p>
             </div>
           </div>
         <figure className="px-4">
-          <img src="https://picsum.photos/id/1005/400/250" className="h-56 object-cover rounded-xl" />
+          <img src="https://source.unsplash.com/random/300x200" className="object-cover card-image" />
         </figure>  
         <div className="card-body">
           <h2 className="card-title font-heading h6">Title of my NFT</h2> 
-          <p className="body-large">Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
+          <p className="card-text body-large">Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
           <div className="justify-end card-details">
             <p className="font-body caption-large text-base-300"> Current price</p> 
             <p className="pl-2 h6 font-bold gradient-four">0.80 ETH</p>
@@ -28,13 +28,13 @@ function NFT() {
         <div className="card-user">
             <div class="avatar items-center">
               <div class="rounded-full w-7 h-7">
-                <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png" />
+                <img src="https://source.unsplash.com/random/300x300" />
               </div>
               <p className="body-large ml-2 text-base-content">@username</p>
             </div>
           </div>
         <figure className="px-4">
-          <img src="https://picsum.photos/id/1005/400/250" className="h-80 object-cover rounded-xl" />
+          <img src="https://source.unsplash.com/random/300x400" className="object-cover card-image" />
         </figure>  
         <div className="card-body">
           <h2 className="card-title font-heading h6">Title of my NFT</h2> 
@@ -46,12 +46,12 @@ function NFT() {
       </div>
       <div className="card bordered">
         <figure className="card-image">
-          <img src="https://picsum.photos/id/1005/400/250" className="h-80 object-cover rounded-xl" />
+          <img src="https://source.unsplash.com/random/380x400" className="object-cover card-image" />
         </figure> 
         <div className="card-body">
         <div class="avatar">
               <div class="rounded-full w-6 h-6">
-                <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png" />
+                <img src="https://source.unsplash.com/random/300x300" />
               </div>
               <p className="ml-2 body-large text-base-content">@username</p>
             </div>
--- docs/src/components/cards/TokenGatedCard.js
+++ docs/src/components/cards/TokenGatedCard.js
@@ -4,20 +4,20 @@ function TokenGatedCard() {
   return (
     <div className="flex space-x-4">
       <div className="card bordered">
-        <figure className="card-image">
-          <img class="object-cover h-full rounded-2xl" src="https://source.unsplash.com/random/300x360"/>
+        <figure>
+          <img class="object-cover h-full card-image" src="https://source.unsplash.com/random/300x360"/>
             <div class="justify-right absolute left-52 top-6">
               <p class="badge badge-ghost caption-bold">🔒 45 TKN</p>
             </div>
             <div class="card-button-overlay-cb">
-              <p class="btn btn-gradient w-2/4 button-small">Access</p>
+              <p class="btn btn-gradient w-2/4 text-primary-content button-small">Access</p>
             </div>
         </figure>
         <div className="card-body">
           <h2 className="card-title font-heading h5">Title of my NFT</h2> 
             <div class="avatar">
               <div class="rounded-full w-6 h-6">
-                <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png" />
+                <img src="https://source.unsplash.com/random/300x300" />
               </div>
               <p className="ml-2 body-large text-base-content">@username</p>
         </div>
