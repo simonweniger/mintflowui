41b2a1e7d2834499a775582961a62ccaa814b807
Simon Weniger <hello@simonweniger.com>
2021-05-31 16:00:11 +0200

    updated font-style and fixed hero secition  (work in progress)

--- docs/src/components/heros/HeroFlex.js
+++ docs/src/components/heros/HeroFlex.js
@@ -5,13 +5,13 @@ function HeroFlex() {
     <div className="hero min-h-screen bg-base-200">
       <div className="flex-col hero-content lg:flex-row-reverse">
         <img src="https://picsum.photos/id/1005/600/600" className="max-w-sm rounded-lg shadow-2xl" /> 
-        <div>
-          <h1 className="mb-5 text-5xl font-bold">
-                Hello there
-              </h1> 
-          <p className="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-              </p> 
+        <div className="hero-content-left">
+          <h1 className="mb-5 hero">
+            Hello there
+          </h1> 
+          <p className="mb-5 body-large">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p> 
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
--- docs/src/components/heros/HeroForm.js
+++ docs/src/components/heros/HeroForm.js
@@ -4,11 +4,11 @@ function HeroForm() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="flex-col justify-center hero-content lg:flex-row">
-        <div className="text-center lg:text-left">
-          <h1 className="mb-5 text-5xl font-bold">
+        <div className="text-left lg:text-left">
+          <h1 className="mb-5 hero">
                 Hello there
               </h1> 
-          <p className="mb-5">
+          <p className="mb-5 body-large">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
               </p>
         </div> 
