5e2289ff1d30bd68cadc5039d9bbda83489b7b1b
Simon Weniger <hello@simonweniger.com>
2021-05-31 14:13:44 +0200

    updated highlited input style

--- docs/src/components/inputs/InputHighlited.js
+++ docs/src/components/inputs/InputHighlited.js
@@ -3,14 +3,11 @@ import React from "react";
 function InputHighlited() {
   return (
     <div class="form-control">
-      <label className="label">
-        <span className="font-heading font-semibold label-text">Enter Link To Gate</span>
-      </label> 
-        <div className="input input-highlited relative  ">
+        <div className="input input-highlited relative">
           <input 
           type="text" 
           placeholder="Paste the link you want to token gate" className="focus:outline-none body-large" /> 
-          <button className="btn btn-primary justify-end">Next</button>
+          <button className="btn btn-primary btn-glow button-small justify-end">Next Step</button>
         </div>
     </div>
   );
