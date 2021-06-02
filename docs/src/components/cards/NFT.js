a1dc7d99f7bab82a124a8001f62d181098f801b0
Simon Weniger <hello@simonweniger.com>
2021-06-02 09:40:58 +0200

    bug fixes and cleanup

--- docs/src/components/cards/NFT.js
+++ docs/src/components/cards/NFT.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function NFT() {
   return (
-    <div className="flex space-x-4">
+    <div className="flex space-x-8">
       <div className="card bordered max-w-xs">
         <div className="card-user">
             <div className="avatar">
               <div className="rounded-full w-7 h-7">
                 <img src="https://source.unsplash.com/random/300x300" />
               </div>
-              <p className="ml-2 body-large text-base-content">@username</p>
+              <p className="ml-2 bodytext">@username</p>
             </div>
           </div>
         <figure className="px-4">
@@ -20,27 +20,7 @@ function NFT() {
           <p className="card-text body-large">Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
           <div className="justify-end card-details">
             <p className="font-body caption-large text-base-300"> Current price</p> 
-            <p className="pl-2 h6 font-bold gradient-four">0.80 ETH</p>
-          </div>
-        </div>
-      </div>
-      <div className="card bordered">
-        <div className="card-user">
-            <div class="avatar items-center">
-              <div class="rounded-full w-7 h-7">
-                <img src="https://source.unsplash.com/random/300x300" />
-              </div>
-              <p className="body-large ml-2 text-base-content">@username</p>
-            </div>
-          </div>
-        <figure className="px-4">
-          <img src="https://source.unsplash.com/random/300x400" className="object-cover card-image" />
-        </figure>  
-        <div className="card-body">
-          <h2 className="card-title font-heading h6">Title of my NFT</h2> 
-          <div className="justify-start card-details">
-            <p className="font-body caption-large text-base-300"> Current price</p> 
-            <p className="pl-2 h6 gradient-four">0.80 ETH</p>
+            <p className="pl-2 title font-bold gradient-four">0.80 ETH</p>
           </div>
         </div>
       </div>
@@ -53,12 +33,12 @@ function NFT() {
               <div class="rounded-full w-6 h-6">
                 <img src="https://source.unsplash.com/random/300x300" />
               </div>
-              <p className="ml-2 body-large text-base-content">@username</p>
+              <p className="ml-2 bodytext">@username</p>
             </div>
           <h2 className="card-title font-heading h6">Title of my NFT</h2> 
           <div className="card-details">
             <p className="font-body caption-large text-base-300"> Current price</p> 
-            <p className="pl-2 h6 gradient-four">0.80 ETH</p>
+            <p className="pl-2 title gradient-four">0.80 ETH</p>
           </div>
         </div>
       </div>
@@ -71,12 +51,12 @@ function NFT() {
               <div class="rounded-full w-6 h-6">
                 <img src="https://source.unsplash.com/random/300x300" />
               </div>
-              <p className="ml-2 body-large text-base-content">@username</p>
+              <p className="ml-2 bodytext">@username</p>
             </div>
           <h2 className="card-title font-heading h6">Title of my NFT</h2> 
           <div className="card-details">
             <p className="font-body caption-large text-base-300"> Current price</p> 
-            <p className="pl-2 h6 gradient-four">0.80 ETH</p>
+            <p className="pl-2 title gradient-four">0.80 ETH</p>
           </div>
         </div>
       </div>
--- docs/src/components/cards/TokenGatedCard.js
+++ docs/src/components/cards/TokenGatedCard.js
@@ -6,20 +6,20 @@ function TokenGatedCard() {
       <div className="card bordered">
         <figure>
           <img class="object-cover h-full card-image" src="https://source.unsplash.com/random/300x360"/>
-            <div class="justify-right absolute left-52 top-6">
-              <p class="badge badge-ghost caption-bold">🔒 45 TKN</p>
+            <div class="justify-right absolute left-52 top-4">
+              <p class="badge badge-ghost font-bold">🔒 45 TKN</p>
             </div>
-            <div class="card-button-overlay-cb">
-              <p class="btn btn-gradient w-2/4 text-primary-content button-small">Access</p>
+            <div class="inline-flex items-center justify-center px-4 absolute bottom-36">
+              <p class="btn btn-gradient w-1/3 text-primary-content buttontext-small">Access</p>
             </div>
         </figure>
         <div className="card-body">
-          <h2 className="card-title font-heading h5">Title of my NFT</h2> 
+          <h2 className="card-title font-heading h6">Title of my NFT</h2> 
             <div class="avatar">
               <div class="rounded-full w-6 h-6">
                 <img src="https://source.unsplash.com/random/300x300" />
               </div>
-              <p className="ml-2 body-large text-base-content">@username</p>
+              <p className="ml-2 body-large bodytext">@username</p>
         </div>
         </div>
       </div>
@@ -27,19 +27,19 @@ function TokenGatedCard() {
         <figure>
           <img class="object-cover h-full card-image-padding" src="https://source.unsplash.com/random/300x360"/>
             <div class="justify-right absolute left-52 top-6">
-              <p class="badge badge-ghost caption-bold">🔒 45 TKN</p>
+              <p class="badge badge-ghost font-bold">🔒 45 TKN</p>
             </div>
             <div class="card-button-overlay-cb">
-              <p class="btn btn-gradient w-2/4 text-primary-content button-small">Access</p>
+              <p class="btn btn-gradient w-1/3 text-primary-content buttontext-small">Access</p>
             </div>
         </figure>
         <div className="card-body">
-          <h2 className="card-title font-heading h5">Title of my NFT</h2> 
+          <h2 className="card-title font-heading h6">Title of my NFT</h2> 
             <div class="avatar">
               <div class="rounded-full w-6 h-6">
                 <img src="https://source.unsplash.com/random/300x300" />
               </div>
-              <p className="ml-2 body-large text-base-content">@username</p>
+              <p className="ml-2 body-large bodytext">@username</p>
         </div>
         </div>
       </div>
