5a0d390efd3c60bfc14e3f24c1fbc92d23e4abb5
Simon Weniger <hello@simonweniger.com>
2021-09-29 16:17:42 +0200

    huge style update for mintflow 0.4

--- docs/src/pages/InputList.js
+++ docs/src/pages/InputList.js
@@ -15,43 +15,43 @@ function InputList() {
         <div className="overflow-auto h-screen pb-44 pt-2 pr-2 pl-2 md:pt-0 md:pr-0 md:pl-0">
                 <div className="w-full space-y-10">
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input</h1>
                         <Input />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input InputHighlited</h1>
                         <InputHighlited />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input without border</h1>
                         <InputNoBorder />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input Ghost</h1>
                         <InputGhost />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input Disabled</h1>
                         <InputDisabled />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input Brand Colors</h1>
                         <InputBrandColors />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input States</h1>
                         <InputStates />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input Helper</h1>
                         <InputHelper />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input Sizes</h1>
                         <InputSizes />                                   
                     </div>
                     <div className="mb-4 bg-base-100 p-6 rounded-xl">
-                        <h1>Cards</h1>
+                        <h1>Input Button</h1>
                         <InputButton />                                   
                     </div>
                 </div>
