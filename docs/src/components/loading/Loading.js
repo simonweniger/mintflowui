792d6e7077efd6b7690a6dd8562679026c067473
Simon Weniger <hello@simonweniger.com>
2021-11-04 12:06:38 +0100

    button updates

--- docs/src/components/loading/Loading.js
+++ docs/src/components/loading/Loading.js
@@ -4,16 +4,9 @@ function Loading() {
   return (
     <div class="p-6 space-y-2 artboard phone">
       <div class="loading-default"></div> 
-      <div class="loading-circle"></div> 
-      <div class="loading-dual-ring" ></div> 
-      <div class="loading-facebook"></div> 
-      <div class="loading-heart"></div>
-      <div class="loading-ring"></div>
-      <div class="loading-roller"></div>
-      <div class="loading-ellipsis"></div>
-      <div class="loading-ripple"></div>
+      <div class="loading-default-primary"></div> 
     </div>
     );
 }
                 
-export default Progress;
+export default Loading;
